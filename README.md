53d0e0714bb13b66db697a7a6b7d6a3dcda3434e
ctf0 <muah003@gmail.com>
2020-01-30 00:23:56 +0200

    001

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-# Reveal File In Folder
-
-> Quickly open your file in folder by context menu.
-
-## Features
-
-It provides a context menu for you to open a exist file in folder.
-
-### Screenshots
-
-![](https://raw.githubusercontent.com/Molunerfinn/test/master/picgo/RevealFileinFolder.gif)
+based on https://github.com/Molunerfinn/VSCode-RevealFileinFolder + better check for opened files
--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,39 @@
 'use strict'
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate (context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
+    setContext(false)
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "revealfileinfolder" is now active!')
+    // current
+    let editor = vscode.window.activeTextEditor
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('extension.revealFileInFolder', () => {
-    // The code you place here will be executed every time your command is executed
+    if (editor && isAFile(editor)) {
+        setContext(true)
+    }
 
-    // Display a message box to the user
-    // vscode.window.showInformationMessage('Hello World!')
-    vscode.commands.executeCommand('revealFileInOS')
-  })
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+        setContext(false)
 
-  context.subscriptions.push(disposable)
+        if (editor && isAFile(editor)) {
+            setContext(true)
+        }
+    })
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.revealFileInFolder', () => {
+            vscode.commands.executeCommand('revealFileInOS')
+        })
+    )
 }
 
-// this method is called when your extension is deactivated
-export function deactivate () {
-  console.log('Deactivate')
+function isAFile({ document }: any) {
+    return document.fileName.includes('/')
+}
+
+function setContext(val: any) {
+    return vscode.commands.executeCommand('setContext', 'canRevealFile', val)
+}
+
+export function deactivate() {
 }
